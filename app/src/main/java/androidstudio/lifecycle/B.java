1fb0326f2f2090f303a208393baf8b0e695bbf73
hardikganatra01 <hardikganatra01@gmail.com>
2016-12-23 13:06:02 +0530

    Initial commit

---
+++ app/src/main/java/androidstudio/lifecycle/B.java
@@ -0,0 +1,15 @@
+package androidstudio.lifecycle;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Bhavya Thacker on 21-12-2016.
+ */
+public class B extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_b);
+    }
+}
